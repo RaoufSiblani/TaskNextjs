8bdcfabaabb8114e951f7ca4bf2dd0b27ab6c190
RaoufSiblani <siblani.raouf13@gmail.com>
2023-06-22 11:23:29 +0300

    Update MenuItem.js

--- comp/MenuItem.js
+++ comp/MenuItem.js
@@ -1,7 +1,7 @@
 const MenuItem = ({ image, title, price, description }) => {
   return (
     <div className="inline-block  w-30 sm:w-45 lg:w-90 mx-10 my-5 border-2 border-black rounded-25 p-1 box-border">
-      <img src={image} alt="Food" className="w-95 object-contain rounded-25" />
+      <img src={image} alt="Food" className="w-95 object-contain rounded-25 px-1" />
       <div className="text-left mt-10">
         <h3>{title}</h3>
         <h4>{price}$</h4>
