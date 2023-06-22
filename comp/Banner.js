d61e823e2fa0736d34baf7e055e1e7d990bbe366
RaoufSiblani <siblani.raouf13@gmail.com>
2023-06-22 10:22:53 +0300

    Update Banner.js

--- comp/Banner.js
+++ comp/Banner.js
@@ -3,8 +3,8 @@ import Image from 'next/image';
 
 const Banner = ({ imageSrc, overlayText }) => {
   return (
-    <div className="flex justify-center w-full relative h-screen max-h-800">
-       <Image src={imageSrc} alt="Banner"  width={1800} height={20} className=" rounded-lg object-cover"/>
+    <div className="flex justify-center w-full relative">
+       <Image src={imageSrc} alt="Banner"  width={1800} height={800} className=" rounded-lg object-cover w-90 h-50"/>
       <div className="overlay absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <p className="font-sans w-full text-center font-bold text-5xl break-words text-black relative ">
           {overlayText}
