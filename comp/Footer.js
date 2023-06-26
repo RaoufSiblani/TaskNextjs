22c706d26a1ea599b0c86bd09b3f3e954b852807
RaoufSiblani <siblani.raouf13@gmail.com>
2023-06-26 13:05:58 +0300

    Update Footer.js

--- comp/Footer.js
+++ comp/Footer.js
@@ -4,14 +4,14 @@ const Footer = () => {
     <footer>
       <div className="relative flex justify-center items-center flex-col w-full bg-black mt-5 pb-3">
         <div className="flex justify-center">
-          <div className="p-10"><a className="inline-block h-10 w-10 bg-white text-black text-center leading-10 rounded-full transition-colors duration-500 ease-in-out hover:text-yellow-400" href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></div>
-          <div className="p-10"><a className="inline-block h-10 w-10 bg-white text-black text-center leading-10 rounded-full transition-colors duration-500 ease-in-out hover:text-yellow-400" href="#" target="_blank"><i class="fab fa-twitter"></i></a></div>
-          <div className="p-10"><a className="inline-block h-10 w-10 bg-white text-black text-center leading-10 rounded-full transition-colors duration-500 ease-in-out hover:text-yellow-400" href="#" target="_blank"><i class="fab fa-instagram"></i></a></div>
+          <div className="p-10"><a className="inline-block h-10 w-10 bg-white  leading-10 rounded-full transition-colors duration-500 ease-in-out hover:bg-orange213" href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></div>
+          <div className="p-10"><a className="inline-block h-10 w-10 bg-white  leading-10 rounded-full transition-colors duration-500 ease-in-out hover:bg-orange213" href="#" target="_blank"><i class="fab fa-twitter"></i></a></div>
+          <div className="p-10"><a className="inline-block h-10 w-10 bg-white  leading-10 rounded-full transition-colors duration-500 ease-in-out hover:bg-orange213" href="#" target="_blank"><i class="fab fa-instagram"></i></a></div>
         </div>
 
         <div className="div2">
           <h3 className="text-white text-base text-center inline">Also visit: </h3>
-          <a className="no-underline text-white text-base text-center inline hover:text-yellow-400" href="https://en.wikipedia.org/wiki/Bleach_(manga)" target="_blank" > Wiki</a>
+          <a className="no-underline text-white text-base text-center inline hover:text-orange213" href="https://en.wikipedia.org/wiki/Bleach_(manga)" target="_blank" > Wiki</a>
         </div>
       </div>
     </footer>
