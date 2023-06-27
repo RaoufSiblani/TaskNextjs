c61f4611049e33f4c61ca9a80ce095470951cc41
RaoufSiblani <siblani.raouf13@gmail.com>
2023-06-27 11:27:36 +0300

    swiper

--- comp/Carousel.js
+++ comp/Carousel.js
@@ -1,32 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+
 import Banner from './Banner';
+import SwiperCore, { Navigation } from 'swiper';
+
+SwiperCore.use([Navigation]);
 
 const Carousel = ({ bannerData }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const swiperRef = useRef(null);
 
   const previousImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? bannerData.length - 1 : prevIndex - 1));
+    swiperRef.current.swiper.slidePrev();
   };
 
   const nextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex === bannerData.length - 1 ? 0 : prevIndex + 1));
+    swiperRef.current.swiper.slideNext();
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex === bannerData.length - 1 ? 0 : prevIndex + 1));
+      nextImage();
     }, 5000);
 
     return () => {
       clearInterval(interval);
     };
-  }, [bannerData]);
+  }, []);
 
   return (
-    <div className="flex w-full relative" >
-      <Banner imageSrc={bannerData[currentImageIndex].imageSrc} overlayText={bannerData[currentImageIndex].overlayText} />
-      <button className="font-sans overlay absolute top-1/2 left-1 ml-12 items-center justify-center h-10 w-10 bg-black rounded-full text-white text-4xl hover:text-orange213" onClick={previousImage}> &lt; </button>
-      <button className="font-sans overlay absolute top-1/2 right-1 mr-12 items-center justify-center h-10 w-10 bg-black rounded-full text-white text-4xl hover:text-orange213" onClick={nextImage}> &gt; </button>
+    <div className="flex w-full relative">
+      <Swiper
+        loop={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        onSlideChange={(swiper) => setCurrentImageIndex(swiper.activeIndex)}
+        navigation={{
+          prevEl: '.prev-button',
+          nextEl: '.next-button',
+        }}
+        ref={swiperRef}
+      >
+        {bannerData.map((banner, index) => (
+          <SwiperSlide key={index}>
+            <Banner imageSrc={banner.imageSrc} overlayText={banner.overlayText} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <button className="prev-button" onClick={previousImage}>
+        &lt;
+      </button>
+      <button className="next-button" onClick={nextImage}>
+        &gt;
+      </button>
     </div>
   );
 };
